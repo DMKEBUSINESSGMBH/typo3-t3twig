691047789eeab921fffb2c633d06ceabf9ae555d
RocKordier <eric@ehdev.de>
2016-05-13 11:40:21 +0200

    Add twig extension for mediaFiles - get images from Tx_Rnbase_Domain_Model_Base - get images from generic call

---
+++ Classes/Twig/ImageExtension.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace DMK\T3twig\Twig;
+
+/**
+ * Class ImageExtension
+ *
+ * @category TYPO3-Extension
+ * @package  DMK\T3twig\Twig
+ * @author   Eric Hertwig <dev@dmk-ebusiness.de>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.dmk-ebusiness.de/
+ */
+class ImageExtension extends \Twig_Extension
+{
+	/**
+	 * @return array
+	 */
+	public function getFilters() {
+		return [
+			new \Twig_SimpleFilter('getMediaObjects', [$this, 'getMediaObjects']),
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getFunctions() {
+		return [
+			new \Twig_SimpleFunction('getGenericMediaObject', [$this, 'getGenericMediaObject']),
+		];
+	}
+
+	/**
+	 * Fetches FAL records and return as array of tx_rnbase_model_media
+	 *
+	 * @param \Tx_Rnbase_Domain_Model_Base $model
+	 * @param $refField
+	 *
+	 * @return array[tx_rnbase_model_media]
+	 */
+	public function getMediaObjects(\Tx_Rnbase_Domain_Model_Base $model, $refField='images')
+	{
+		return $this->fetchFiles($model->getTableName(), $model->getUid(), $refField);
+	}
+
+	/**
+	 * @param $table
+	 * @param $uid
+	 * @param string $refField
+	 *
+	 * @return array
+	 */
+	public function getGenericMediaObject($table, $uid, $refField='images')
+	{
+		return $this->fetchFiles($table, $uid, $refField);
+	}
+
+	/**
+	 * @param $table
+	 * @param $uid
+	 * @param $refField
+	 *
+	 * @return array
+	 */
+	protected function fetchFiles($table, $uid, $refField)
+	{
+		return \tx_rnbase_util_TSFAL::fetchFiles($table, $uid, $refField);
+	}
+
+	/**
+	 * Get Extension name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 't3twig_imageExtension';
+	}
+}
