f2bf901427f0bc9a4396bbe4f094a06cedab3023
RocKordier <eric@ehdev.de>
2016-05-20 13:06:51 +0200

    Add LanguageExtension

---
+++ Classes/Twig/LanguageExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DMK\T3twig\Twig;
+use DMK\T3twig\Util\T3twigEnvironment;
+
+/**
+ * Class LanguageExtension
+ *
+ * @category TYPO3-Extension
+ * @package  DMK\T3twig\Twig
+ * @author   Eric Hertwig <dev@dmk-ebusiness.de>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://www.dmk-ebusiness.de/
+ */
+class LanguageExtension extends \Twig_Extension
+{
+	public function getFilters()
+	{
+		return [
+			new \Twig_SimpleFilter('getTranslation', [$this, 'getTranslation'], ['needs_environment' => true]),
+		];	}
+
+	/**
+	 * @param T3twigEnvironment $env
+	 * @param string $label
+	 * @param string $alt
+	 * @param bool $hsc
+	 *
+	 * @return mixed
+	 */
+	public function getTranslation($env, $label, $alt='', $hsc=FALSE)
+	{
+		return $env->getConfigurations()->getLL($label, $alt, $hsc);
+	}
+
+	/**
+	 * Get Extension name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 't3twig_translationExtension';
+	}
+}
