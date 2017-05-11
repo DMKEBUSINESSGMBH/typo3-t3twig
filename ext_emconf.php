8e5b2bda0c8ac1835483567b69ee74f25f912885
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2017-05-11 11:55:50 +0200

    Version 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,38 +9,36 @@
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link     https://www.dmk-ebusiness.de/
  */
-
-$EM_CONF[ $_EXTKEY ] = [
-    'title'                         => 'T3twig',
-    'description'                   => 'TYPO3 extension to render page templates with Twig and extend rn_base for using Twig templates instead of marker ',
-    'shy'                           => 0,
-'version' => '1.2.0',
-    'dependencies'                  => 'cms',
-    'conflicts'                     => '',
-    'priority'                      => '',
-    'loadOrder'                     => '',
-    'module'                        => '',
-    'state'                         => 'stable',
-    'uploadfolder'                  => 0,
-    'modify_tables'                 => '',
-    'clearcacheonload'              => 0,
-    'lockType'                      => '',
-    'category'                      => 'misc',
-    'author'                        => 'DMK E-BUSINESS GmbH',
-    'author_email'                  => 'dev@dmk-ebusiness.de',
-    'author_company'                => 'DMK E-BUSINESS GmbH',
-    'CGLcompliance'                 => '',
-    'CGLcompliance_note'            => '',
-    'constraints'                   => [
-        'depends'   => [
-            'rn_base' => '1.2.5-',
-            'typo3'   => '7.6.16-8.7.99',
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'T3twig',
+    'description' => 'TYPO3 extension to render page templates with Twig and extend rn_base for using Twig templates instead of marker ',
+    'shy' => 0,
+    'version' => '2.0.0',
+    'dependencies' => 'cms',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'category' => 'misc',
+    'author' => 'DMK E-BUSINESS GmbH',
+    'author_email' => 'dev@dmk-ebusiness.de',
+    'author_company' => 'DMK E-BUSINESS GmbH',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => [
+        'depends' => [
+            'rn_base' => '1.4.0-',
+            'typo3' => '7.6.16-8.7.99',
         ],
         'conflicts' => [],
-        'suggests'  => [],
+        'suggests' => []
     ],
     '_md5_values_when_last_written' => '',
-    'suggests'                      => [
-    ],
-    'createDirs'                    => 'typo3temp/t3twig/',
+    'suggests' => [],
+    'createDirs' => 'typo3temp/t3twig/',
 ];
